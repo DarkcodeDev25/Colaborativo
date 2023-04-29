cf34c698eda3fef147dd7ae0b4d72a7cc2583112
Kenshin Himura <KenshinHimura25>
2023-04-28 23:57:27 -0500

    subemelo

---
+++ ColaborativoJDK/src/colaborativojdk/ColaborativoJDK.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package colaborativojdk;
+
+public class ColaborativoJDK {
+
+
+    public static void main(String[] args) {
+        System.out.println("hola nakamas");
+    }
+    
+}
